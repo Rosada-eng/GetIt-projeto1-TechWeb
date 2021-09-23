44ebc41f363f8e9775f8bd838e6dae9ec98474cd
Rosada-eng <guilhermer4@al.insper.edu.br>
2021-09-23 17:19:00 -0300

    start projeto 1b

---
+++ notes/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, primary_key=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Note(models.Model):
+    title   = models.CharField(max_length=60)
+    content = models.TextField()
+    tag     = models.ForeignKey(Tag, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id}. {self.title} ({self.tag})"

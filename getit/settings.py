6fd93d250c1f1c18d5afbf66f44216a40d40c89f
Rosada-eng <guilhermer4@al.insper.edu.br>
2021-09-27 12:44:30 -0300

    altera psycopg2 para psycopg2-binary e adiciona DATABASE_URL

--- getit/settings.py
+++ getit/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os 
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,6 +77,10 @@ WSGI_APPLICATION = 'getit.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 DATABASES = {
     'default': dj_database_url.config(
